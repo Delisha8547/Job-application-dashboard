4b7b0f3f04f53fac6c08ebea165c54a0a5338a30
Delisha8547 <delisha8547@gmail.com>
2026-01-17 21:09:54 +0000

    Update REDIRECT_URI based on environment variable

--- auth.py
+++ auth.py
@@ -3,7 +3,11 @@ from google.oauth2.credentials import Credentials
 import os
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
-REDIRECT_URI = "http://127.0.0.1:8000/oauth2callback"
+if "RENDER" in os.environ:
+    REDIRECT_URI = "https://job-application-dashboardd.onrender.com/oauth2callback"
+else:
+    REDIRECT_URI = "http://127.0.0.1:8000/oauth2callback"
+
 
 def get_flow():
     return Flow.from_client_secrets_file(
@@ -20,5 +24,6 @@ def load_credentials():
     if os.path.exists('token.json'):
         return Credentials.from_authorized_user_file('token.json', SCOPES)
     return None
+
 
 
